01dae971dd5a4763d189fdd4d07f85b0ea763dce
Fernando Muzaber <fmuzaber@economicas.uba.ar>
2023-11-27 19:27:43 -0300

    simpsons update

--- src/App.tsx
+++ src/App.tsx
@@ -14,16 +14,14 @@ function App() {
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src="../public/simpsons.webp" alt="" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + Zustand + Typescript SWC</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
