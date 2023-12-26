fef084bc4522409fca5ab7e77cc4d9bc73b9570d
Radin6 <nicoramos647@hotmail.com>
2023-12-26 17:53:43 +0100

    personality test page working

--- src/mocks/preguntasPersonalidad.js
+++ src/mocks/preguntasPersonalidad.js
@@ -1,29 +1,92 @@
 export const preguntasPersonalidad = [
   {
     id: 0,
-    pregunta: "pregunta 1",
+    pregunta: "¿Cómo te describirías en situaciones sociales?",
     opciones: [
-      "opción1 de pregunta1",
-      "opción2 de pregunta1",
-      "opción3 de pregunta1",
+      "Muy tímido/a e introvertido/a",
+      "Ambivalente, depende del contexto",
+      "Muy extrovertido/a",
     ],
   },
   {
     id: 1,
-    pregunta: "pregunta 2",
+    pregunta: "¿Cómo manejas el estrés o la presión?",
     opciones: [
-      "opción1 de pregunta2",
-      "opción2 de pregunta2",
-      "opción3 de pregunta2",
+      "Me abrumo fácilmente",
+      "Suelo mantener la calma",
+      "Me encanta el desafío y me motiva",
     ],
   },
   {
     id: 2,
-    pregunta: "pregunta 3",
+    pregunta: "¿Qué tan organizado/a eres en tu vida diaria?",
     opciones: [
-      "opción1 de pregunta3",
-      "opción2 de pregunta3",
-      "opción3 de pregunta3",
+      "Desorganizado/a",
+      "Organizado/a en general",
+      "Muy organizado/a",
     ],
   },
+  {
+    id: 3,
+    pregunta: "¿Cómo enfrentas los cambios en tu vida?",
+    opciones: [
+      "Me cuesta adaptarme",
+      "Adaptabilidad moderada",
+      "Busco activamente nuevos desafíos y cambios",
+    ],
+  },
+  {
+    id: 4,
+    pregunta: "¿Qué tan optimista o pesimista eres por naturaleza?",
+    opciones: ["Muy pesimista", "Neutro/a o equilibrado/a", "Muy optimista"],
+  },
 ];
+{
+  /*
+¿Cómo te describirías en situaciones sociales?
+
+(1) Muy tímido/a e introvertido/a
+(2) Moderadamente reservado/a
+(3) Ambivalente, depende del contexto
+(4) Bastante extrovertido/a
+(5) Muy extrovertido/a
+
+¿Cómo manejas el estrés o la presión?
+
+(1) Me abrumo fácilmente
+(2) A veces me afecta, pero puedo manejarlo
+(3) Suelo mantener la calma
+(4) Me desempeño bien bajo presión
+(5) Me encanta el desafío y me motiva
+
+¿Qué tan organizado/a eres en tu vida diaria?
+
+(1) Desorganizado/a
+(2) Algo desorganizado/a
+(3) Organizado/a en general
+(4) Bastante organizado/a
+(5) Muy organizado/a
+
+¿Cómo enfrentas los cambios en tu vida?
+
+(1) Me cuesta adaptarme
+(2) Prefiero la estabilidad, pero puedo adaptarme
+(3) Adaptabilidad moderada
+(4) Me gusta el cambio y lo abrazo
+(5) Busco activamente nuevos desafíos y cambios
+
+¿Qué tan optimista o pesimista eres por naturaleza?
+
+(1) Muy pesimista
+(2) Moderadamente pesimista
+(3) Neutro/a o equilibrado/a
+(4) Moderadamente optimista
+(5) Muy optimista
+---
+Resulados
+
+0-8   Introvertido
+9-16  Intermedio
+17-25 Extrovertido
+*/
+}
--- src/pages/Personality.jsx
+++ src/pages/Personality.jsx
@@ -9,23 +9,39 @@ import {
   Radio,
   RadioGroup,
   Stack,
+  Spacer,
 } from "@chakra-ui/react";
 import { preguntasPersonalidad } from "../mocks/preguntasPersonalidad";
 
 export default function Personality() {
-  const [answers, setAnswers] = useState({});
+  const [result, setResult] = useState(0);
   const [counter, setCounter] = useState(0);
   const [value, setValue] = useState();
 
   // Función para manejar el envío del formulario
-  const handleSubmit = () => {
-    alert("Gracias por realizar las preguntas de Personalidad");
-    console.log(answers);
+  const handleSubmit = async () => {
+    const lastResutl = result + value;
+    await setResult(lastResutl);
+    await alert(
+      `Gracias por realizar las preguntas de Personalidad, su personalidad es: ${
+        lastResutl < 6
+          ? "Introvertido"
+          : lastResutl > 9
+          ? "Extrovertido/a"
+          : "Intermedio/a"
+      }`
+    );
   };
 
   return (
     <Center mt="5%">
-      <Card maxW={["90%", "400px"]} textAlign="center" bg="#59c9a5">
+      <Card
+        maxW={["90%", "400px"]}
+        minH="300px"
+        textAlign="center"
+        bg="#59c9a5"
+        p="1em"
+      >
         <Heading>Test de Personalidad</Heading>
         <Text>Por favor, responde las siguientes preguntas:</Text>
 
@@ -33,25 +49,43 @@ export default function Personality() {
           <Text>{preguntasPersonalidad[counter].pregunta}</Text>
           <RadioGroup value={value} onChange={(e) => setValue(parseInt(e))}>
             <Stack direction="column">
-              <Radio value={1}>
-                {preguntasPersonalidad[counter].opciones[0]}
-              </Radio>
-              <Radio value={2}>
-                {preguntasPersonalidad[counter].opciones[1]}
-              </Radio>
-              <Radio value={3}>
-                {preguntasPersonalidad[counter].opciones[2]}
-              </Radio>
+              {preguntasPersonalidad[counter].opciones.map((op) => (
+                <Radio
+                  value={
+                    preguntasPersonalidad[counter].opciones.indexOf(op) + 1
+                  }
+                  key={op}
+                >
+                  {op}
+                </Radio>
+              ))}
             </Stack>
           </RadioGroup>
         </Box>
+        <Spacer />
 
-        {counter === 2 ? (
-          <Button onClick={handleSubmit} bg="#63B31D">
+        {counter === preguntasPersonalidad.length - 1 ? (
+          <Button
+            onClick={handleSubmit}
+            colorScheme="purple"
+            size="md"
+            w="fit-content"
+            alignSelf="center"
+          >
             Enviar
           </Button>
         ) : (
-          <Button onClick={() => setCounter(counter + 1)} bg="#d5395e">
+          <Button
+            onClick={() => {
+              setCounter(counter + 1);
+              setResult(result + value);
+              setValue();
+            }}
+            colorScheme="facebook"
+            size="md"
+            w="fit-content"
+            alignSelf="center"
+          >
             Siguiente
           </Button>
         )}
